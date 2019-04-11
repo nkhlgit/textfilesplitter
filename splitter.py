726fcebd4632b1ceef8bb092fe2c27f9f390444a
nkhlgit <nikhil.eltrx@gmail.com>
2019-04-11 20:05:06 +1000

    first commit

---
+++ splitter.py
@@ -0,0 +1,34 @@
+import sys
+
+def main(argv):
+    input_file_name = argv[0]
+    num_output_files = int(argv[1])
+    with open(input_file_name, 'r') as f:
+        for total_lines, l in enumerate(f):
+            pass
+    print(total_lines)
+    # minus 1 becase to last file contain remender after diviion.
+    lines_a_file = total_lines // (num_output_files - 1)
+
+    in_file = open(input_file_name, 'r')
+    k= 0
+    while (k <= num_output_files ):
+        k += 1
+        output_file_name = input_file_name + '_' + str(k)
+        out_file = open(output_file_name, 'w')
+        for m, line in enumerate(in_file):
+            if (m < lines_a_file):
+                out_file.write(line)
+            else:
+                out_file.write(line)
+                break
+        out_file.close()
+    in_file.close()
+
+
+if __name__ == "__main__":
+     if len(sys.argv) != 3:
+         print(" enter file name and number of output files in format:")
+         sys.exit(0)
+
+     main(sys.argv[1:])
