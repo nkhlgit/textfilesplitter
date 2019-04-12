5eb48e7918bfa366551b801e8765fab1cb747e98
Nikhil Kumar Singh <nikhil.eltrx@gmail.com>
2019-04-12 10:34:54 +1000

    Update splitter.py
    added encoding while opening the file

--- splitter.py
+++ splitter.py
@@ -3,19 +3,19 @@ import sys
 def main(argv):
     input_file_name = argv[0]
     num_output_files = int(argv[1])
-    with open(input_file_name, 'r') as f:
+    with open(input_file_name, 'r', encoding="utf8") as f:
         for total_lines, l in enumerate(f):
             pass
     print(total_lines)
     # minus 1 becase to last file contain remender after diviion.
     lines_a_file = total_lines // (num_output_files - 1)
 
-    in_file = open(input_file_name, 'r')
+    in_file = open(input_file_name, 'r', encoding="utf8")
     k= 0
     while (k <= num_output_files ):
         k += 1
         output_file_name = input_file_name + '_' + str(k)
-        out_file = open(output_file_name, 'w')
+        out_file = open(output_file_name, 'w', encoding="utf8")
         for m, line in enumerate(in_file):
             if (m < lines_a_file):
                 out_file.write(line)
